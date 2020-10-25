56655e40cecb9ed6f21f5debe7ed3436db68ba5a
Floozutter <floozutter@gmail.com>
2020-10-25 02:29:48 -0700

    Expect error codes to always be negative

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 
 int main(int argc, char * argv[]) {
 	// Initialize SDL.
-	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
         return 1;
 	}
